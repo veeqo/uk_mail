d16d440bba1a9880c7ea38a8cd89e3b3d068fc5b
Luke Farmer <luke.e.farmer@gmail.com>
2015-09-29 15:35:07 +0100

    Add Collection service

--- lib/uk_mail.rb
+++ lib/uk_mail.rb
@@ -1,6 +1,8 @@
 require "uk_mail/version"
 require 'uk_mail/session'
 
+require 'uk_mail/soap_service/collection'
+
 module UKMail
   # Your code goes here...
 end
--- lib/uk_mail/service/collection.rb
+++ lib/uk_mail/service/collection.rb
@@ -4,7 +4,28 @@ require 'uk_mail/session'
 module UKMail
   module Service
     class CollectionService < Base
-      # --
+      def soap_namespace
+        SoapService::Collection
+      end
+
+      def soap_service
+        soap_namespace::IUKMCollectionService
+      end
+
+      def book_collection(params = {})
+        service.bookCollection(soap_namespace::BookCollection.new(soap_namespace::BookCollectionWebRequest.new(
+
+          @session.auth_token,
+          params[:username],
+          params[:account_number],
+          params[:closed_for_lunch],
+          params[:earliest_time],
+          params[:latest_time],
+          params[:requested_collection_date],
+          params[:special_instructions]
+
+        ))).bookCollectionResult
+      end
     end
   end
 end
