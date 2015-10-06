c4c8f1c9cde68e52c3dc62b6b6930fadf82a656c
Luke Farmer <luke.e.farmer@gmail.com>
2015-10-06 10:22:26 +0100

    Clean up spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 require 'minitest/autorun'
 require 'uk_mail'
-require 'byebug'
 
 UKMail.configure do |config|
   spec_path = File.dirname(__FILE__)
+  config.env = :test
   config.postcode_dat_path = spec_path + '/fixtures/postcode_sample.dat'
 end
